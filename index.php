b335370d79d8b3510f4bfe05a2f2d8f75144c948
Greg1928 <gregoriosortino@protonmail.com>
2022-06-23 16:35:03 +0200

    add header

--- index.php
+++ index.php
@@ -31,9 +31,35 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>google-faq</title>
     <link rel="stylesheet" href="style.css">
+    <!-- FontAwesom cdn -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
 <body>
-    <header></header>
+    <header>
+        <div class="content">
+            <div class="top">
+                <div class="left">
+                    <img src="img/purepng.com-google-logo-2015brandlogobrand-logoiconssymbolslogosgoogle-6815229372333mqrr.webp" alt="">
+                    <span>Privacy e termini</span>
+                </div>
+                <div class="right">
+                    <i class="fa-solid fa-bars"></i>
+                    <i class="fa-solid fa-circle-user"></i>
+                </div>
+            </div>
+            <div class="bottom">
+                <nav>
+                    <ul>
+                        <li><a href="#">Introduzione</a></li>
+                        <li><a href="#">Norme sulla privacy</a></li>
+                        <li><a href="#">Termini di servizio</a></li>
+                        <li><a href="#">Tecnologie</a></li>
+                        <li><a href="#">Domande frequenti</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+    </header>
     <main>
         <div class="container">
             <?php foreach($data as $info) { ?>
